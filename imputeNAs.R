bad9bb2cfea67f7d93a50be39d6485ade30e2fca
Jeff Allen <jeff.allen@trestletechnology.net>
2012-04-09 19:48:01 -0500

    Added RUnit tests for the function, ran tests and it failed on any first or last element NAs. Had to expand window size to 2X to account for that scenario.

--- imputeNAs.R
+++ imputeNAs.R
@@ -5,6 +5,19 @@
 #' @return the vector with the NAs having been replaced by the nearest non-NA value
 #' @author Jeffrey D. Allen \email{jeff.allen@@trestletechnology.net}
 imputeNAs <- function(dat){
+  
+  if(is.null(dat)){
+    return(NULL);
+  }
+  
+  if (all(is.na(dat))){
+    stop("Can't impute NAs if there are no non-NA values.")
+  }
+  
+  if (!any(is.na(dat))){
+    return(dat);
+  }
+  
   #get the index of all NA values
   nas <- which(is.na(dat))
 
@@ -15,7 +28,9 @@ imputeNAs <- function(dat){
   length <- getLengthNAs(dat);
 
   #the furthest away an NA value could be is half of the length of the maximum NA run
-  windowSize <- ceiling(length/2)
+  #if there's a run at the beginning or end, then the nearest non-NA value could possibly be `length` away, so we need to keep the window large for that case.
+  #TODO: if we check the first and last elements separately, then we can use windowSize <- length/2
+  windowSize <- length
 
   #loop through all NAs
   for (thisIndex in nas){
@@ -40,4 +55,55 @@ imputeNAs <- function(dat){
   }
   dat
 }
+
+runit.testFirstNA <- function(){
+  x <- c(NA, 1:3)
+  checkIdentical(imputeNAs(x), as.integer(c(1,1:3)))
+}
+
+runit.testLastNA <- function(){
+  x <- c(1:3, NA)
+  checkIdentical(imputeNAs(x), as.integer(c(1:3,3)))
+}
+
+runit.testFirstNARun <- function(){
+  x <- c(NA, NA, NA, NA, 1:3)
+  checkIdentical(imputeNAs(x), as.integer(c(1,1,1,1,1:3)))
+}
+
+runit.testLastNARun <- function(){
+  x <- c(1:3, NA, NA, NA, NA, NA)
+  checkIdentical(imputeNAs(x), as.integer(c(1:3,3,3,3,3,3)))
+}
+
+runit.testSingle <- function(){
+  x <- c(1:3, NA, 1:3)
+  checkIdentical(imputeNAs(x), as.integer(c(1:3,3,1:3)))
+}
+
+runit.testDouble <- function(){
+  x <- c(1:3, NA, NA, 3:1)
+  checkIdentical(imputeNAs(x), as.integer(c(1:3,1,3,1:3)))
+}
+
+runit.testMixed <- function(){
+  x <- c(1:3, NA, 3:4, NA, NA, 5:6, NA, NA, NA, 7:8)
+  checkIdentical(imputeNAs(x), as.integer(c(1:3,3, 3:4, 4, 5, 5:6, 6, 6, 7, 7:8)))
+}
+
+runit.testAllNA <- function(){
+  x <- c(NA, NA, NA)
+  checkException(imputeNAs(x))
+}
+
+runit.testNoNA <- function(){
+  x <- c(1:3)
+  checkIdentical(imputeNAs(x), 1:3)
+}
+
+runit.testEmpty <- function(){
+  x <- c()
+  checkIdentical(imputeNAs(x), NULL)
+}
+
 
